1405c2452624deca3350d2b943061c833f4af2ec
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-09-16 10:39:25 -0700

    add react spinner to goal table

--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRecoilValue, useRecoilState } from 'recoil'
+import { css } from "@emotion/core";
+import PropagateLoader from "react-spinners/PropagateLoader";
 import axios from 'axios'
 import { Table } from '../../styles'
 import GoalCard from './GoalCard'
@@ -8,26 +10,42 @@ import {
   goalListState,
 } from '../../utils/store'
 
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
 
 const TableThing = () => {
   const user = useRecoilValue(userState)
   const [goalList, setGoalList] = useRecoilState(goalListState)
+  const [loading, setLoading] = useState(true)
 
-  async function fetchMyAPI() {
-  await axios.get(`${process.env.REACT_APP_BE}/goals/all/${user.id}`)
-    .then(res => { setGoalList(
-        res.data.sort((a,b) => (a.target_date > b.target_date) ? 1 : -1)
-      )}) // sort by date
+  const getGoals = async() => {
+  await axios.get(`${ process.env.REACT_APP_BE }/goals/all/${ user.id }`)
+    .then(res => {
+      setGoalList(res.data.sort((a,b) => (a.target_date > b.target_date) ? 1 : -1))
+      setLoading(false)
+    }) // sort by date
     .catch(err => { console.log(err) })
   }
 
-  useEffect(() => { fetchMyAPI() }, [goalList.length])
+  useEffect(() => { getGoals() }, [goalList.length])
 
+  return (
 
-    // console.log(user.name, {goalList}, {habitList})
-  return ( <Table>
-    {goalList && goalList.map(g => ( <GoalCard key={g.id} props={g} /> )) }
-  </Table> )
+  <Table>
+    { loading && <PropagateLoader
+      css={ override }
+      size={ 45 }
+      color={ "#123abc" }
+      loading={ loading }
+    /> }
+
+    { goalList && goalList.map(g => (<GoalCard key={ g.id } props={ g } />)) }
+  </Table>
+  )
 }
 
 export default TableThing
