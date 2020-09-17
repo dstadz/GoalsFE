262987063f3eb65ab78bdc4eeb1599b42ce756da
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-09-17 14:28:49 -0700

    tidy up, live update goallist on Edit

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -66,17 +66,13 @@ const LandingPage = () => {
 
         <h2> Work towards becoming the ideal You </h2>
         <p> You'll be lucky if you get <span>{time}</span> </p>
-        <p>
-          Gain perspective and awareness by getting all your aspirations and dreams out of your head and onto your goal list (no matter where you are or what device you use).
-        </p>
+        <p> Gain perspective and awareness by getting all your aspirations and dreams out of your head and onto your goal list (no matter where you are or what device you use). </p>
         <span> Browse Goals features </span>
         <span> See Goals in action </span>
       </Hero>
 
       <ReviewBox>
-        <h2>
-          100 BILLION HUMANS HAVE HAD GOALS BEFORE. WILL YOU JOIN THE FEW THAT HAVE ACHIEVED THEM?
-        </h2>
+        <h2> 100 BILLION HUMANS HAVE HAD GOALS BEFORE. WILL YOU JOIN THE FEW THAT HAVE ACHIEVED THEM? </h2>
         <ul>
           {fakeReviews.map(r => ( <li key={r.reviewer}>{r.reviewer} {r.rating} {r.comment} </li>))}
         </ul>
@@ -85,17 +81,15 @@ const LandingPage = () => {
       <Section>{/*1- */}
         <div>
           <p> Goals has helped me and </p>
-          <h4> a bunch of other people do finish a lot of goals</h4>
-          <h6>nile, fakebook, wetwerk mickey</h6>
+          <h4> a bunch of other people do finish a lot of goals </h4>
+          <h6> nile, fakebook, wetwerk mickey </h6>
         </div>
         <img src={testimonialPic} alt={'testimonial'}/>
       </Section>
 
       <Section>{/*2- */}
         <div>
-          <p>
-            "I was literally a barely functional person who struggled to work towards any form of self improvement or betterment until I started using Goals"
-          </p>
+          <p> "I was literally a barely functional person who struggled to work towards any form of self improvement or betterment until I started using Goals" </p>
         </div>
         <img src={testimonialPic} alt='testiminial'/>
         <ol>
@@ -105,15 +99,9 @@ const LandingPage = () => {
 
       <Section>{/*3- */}
         <div>
-          <h2>
-          A goal manager you can trust for life
-          </h2>
-          <p>
-          In the {'timeSinceInit'} that I've been making Goals, I've been looking for someone to give me a lot of money to work on something else.
-          </p>
-          <p>
-          I am committed to improving my life by getting a job in tech, and earning your trust through this high quality app helps me and you with our goals.
-          </p>
+          <h2> A goal manager you can trust for life </h2>
+          <p> In the {'timeSinceInit'} that I've been making Goals, I've been looking for someone to give me a lot of money to work on something else. </p>
+          <p> I am committed to improving my life by getting a job in tech, and earning your trust through this high quality app helps me and you with our goals. </p>
         </div>
         <img src={finalPic} alt='final'/>
       </Section>
