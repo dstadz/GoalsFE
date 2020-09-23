a4bcc218cda44d7f1641bbadb9f5400e9a0cb714
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-09-23 11:38:35 -0700

    hide countdown from action center

--- src/Components/GoalTable/GoalCard.js
+++ src/Components/GoalTable/GoalCard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useRecoilState } from 'recoil'
 
 import AddHabitForm from '../Forms/AddHabitForm'
 import EditGoalForm from '../Forms/EditGoalForm'
