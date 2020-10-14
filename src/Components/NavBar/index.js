17fe00a82ac84a8ce03fb2e66c08289c6de8295a
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-10-14 08:23:34 -0700

    add nav

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <span>
+      Goal Getter
+      </span>
+      <div>
+        <span> UserName </span>
+        <span> Points </span>
+        <button> dropdown </button>
+        <button> notification Bell </button>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
