8f71a1c7047344156f5c2b64289af58a1bb1bed1
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-10-27 18:10:46 -0700

    add memoization

--- src/Components/TimeLine/index.js
+++ src/Components/TimeLine/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 // import { useRecoilValue } from 'recoil'
 // import moment from 'moment'
 // import { userState } from '../../utils/store'
-import LifeBlocks from './LifeBlocks'
+import MemoizedLifeBlocks from './LifeBlocks'
 import { Top } from '../../styles'
 
 const TimeLine = () => {
   return (
     <Top>
       {/* <h3> Your Life Here:</h3> */}
-      <LifeBlocks />
+      <MemoizedLifeBlocks />
     </Top>
   )
 }
